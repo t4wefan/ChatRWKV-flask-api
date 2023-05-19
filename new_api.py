b1de22ce4f97e9a97dfc69543717bc02a2d612ee
t4wefan <66654403+t4wefan@users.noreply.github.com>
2023-05-19 15:13:27 +0800

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -4,21 +4,21 @@ from rwkv.model import RWKV
 from rwkv.utils import PIPELINE, PIPELINE_ARGS
 import os, sys, torch
 import numpy as np
+np.set_printoptions(precision=4, suppress=True, linewidth=200)
 
 # 加载模型
 print("正在加载模型，请稍等...")
+os.environ['RWKV_JIT_ON'] = '1'
+os.environ["RWKV_CUDA_ON"] = '0' # '1' to compile CUDA kernel (10x faster), requires c++ compiler & cuda libraries
 model = RWKV(model='RWKV-4-Pile-169M-20220807-8023', strategy='cuda fp16')
-args = PIPELINE_ARGS(
-    temperature=1.0,
-    top_p=0.7,
-    top_k=0,  # top_k = 0 则忽略该参数
-    alpha_frequency=0.25,
-    alpha_presence=0.25,
-    token_ban=[0],  # 禁止生成某些 token
-    token_stop=[],  # 遇到指定的 token 就停止生成
-    chunk_len=256  # 将输入分成多个块以节省 VRAM（块越短，速度越慢）
-)
 pipeline = PIPELINE(model, "20B_tokenizer.json")
+
+out, state = model.forward([187, 510, 1563, 310, 247], None)
+print(out.detach().cpu().numpy())                   # get logits
+out, state = model.forward([187, 510], None)
+out, state = model.forward([1563], state)           # RNN has state (use deepcopy to clone states)
+out, state = model.forward([310, 247], state)
+print(out.detach().cpu().numpy())
 
 print("模型加载完成！")
 
@@ -55,7 +55,7 @@ def chat_rwkv():
                          token_ban=[0],  # ban the generation of some tokens
                          token_stop=[],  # stop generation whenever you see any token here
                          chunk_len=256)  # split input into chunks to save VRAM (shorter -> slower)
-    pipeline.generate(ctx, token_count=200, args,)
+    pipeline.generate(ctx, args,)
     # 将模型的输出写入该usrid下的记录列表，并在末尾添加一个换行符
     chat_dict[usrid].append(out + "\n")
     # 将该usrid下的所有记录拼接起来，作为响应返回
